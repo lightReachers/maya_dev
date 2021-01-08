90a4709967ab9ec47c3127de64d6a2acc8f78735
Himanshu Solanki <umt157@gmail.com>
2021-01-08 16:56:24 +0530

    Update maya_open_file.py

--- maya_open_file.py
+++ maya_open_file.py
@@ -49,8 +49,12 @@ class DesignerUI(QtWidgets.QDialog):
         self.ui.layout().setContentsMargins(6, 6, 6, 6)
 
     def create_connections(self):
+        self.ui.open_btn.hide()
         self.ui.cb_prj.currentIndexChanged.connect(self.refresh_episodes)
-        
+        self.ui.epi_list.itemClicked.connect(self.get_shots)
+        self.ui.shot_list.itemClicked.connect(self.get_files)
+        self.ui.file_list.itemClicked.connect(self.get_file_path)
+        self.ui.open_btn.clicked.connect(self.open_maya_scene)
     
     def get_projects(self):
         projects = os.listdir(PRJ_ROOT)
@@ -61,12 +65,48 @@ class DesignerUI(QtWidgets.QDialog):
         current_prj = self.ui.cb_prj.currentText()
         episode_root = os.path.join(PRJ_ROOT, current_prj, "episodes")
         episodes = os.listdir(episode_root)
+        self.ui.epi_list.clear()
+        self.ui.shot_list.clear()
+        self.ui.file_list.clear()
         self.ui.epi_list.addItems(episodes) 
         
     def refresh_episodes(self):
         self.get_episodes()
+        
+    def get_shots(self):
+        self.ui.open_btn.hide()
+        shot_root = os.path.join(PRJ_ROOT, self.ui.cb_prj.currentText(), "episodes", self.ui.epi_list.currentItem().text(), "shot")
+        shots = os.listdir(shot_root)
+        self.ui.shot_list.clear()
+        self.ui.shot_list.addItems(shots)
+        
+    def get_files(self):
+        self.ui.open_btn.hide()
+        current_shot = self.ui.shot_list.currentItem().text()
+        self.file_root = os.path.join(PRJ_ROOT, self.ui.cb_prj.currentText(), "episodes", self.ui.epi_list.currentItem().text(), "shot", current_shot, "lighting\light_char\maya\work")
+        self.ui.file_list.clear()
+        files = os.listdir(self.file_root)
+        for ma_file in files:
+            if ma_file.endswith(".ma"):
+                self.ui.file_list.addItem(ma_file)
     
-   
+    def get_file_path(self):
+        scene_filepath = os.path.join(self.file_root, self.ui.file_list.currentItem().text())
+        self.ui.open_btn.show()
+        return scene_filepath
+        
+    def open_maya_scene(self):
+        scene_file = self.get_file_path()
+        fileCheckState = cmds.file(q=True, modified=True)
+        # cmds.file(new=True, force=True)
+        if fileCheckState: 
+            cmds.file(scene_file, o=True, pmt=True, force=True)
+            self.close() 
+        else:
+            input = cmds.confirmDialog( title='Unsaved changes for current scene', message='Are you sure about open new file?', button=['Yes','No'], defaultButton='Yes', cancelButton='No', dismissString='No' )
+            if input == "Yes":
+               cmds.file(scene_file, o=True, pmt=True, force=True) 
+               self.close() 
 if __name__ == "__main__":
 
     try:
